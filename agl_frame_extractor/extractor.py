c03c125acdddd9179d5ec5f77c9915a412f128aa
Thomas J. Lux <31990770+Maddonix@users.noreply.github.com>
2023-10-17 09:38:08 +0200

    added progress bar

--- agl_frame_extractor/extractor.py
+++ agl_frame_extractor/extractor.py
@@ -1,19 +1,84 @@
 import cv2
-import os
 import json
-from tqdm import tqdm
 import logging
+import os
 from concurrent.futures import ThreadPoolExecutor
+from tqdm import tqdm
+
 
 class VideoFrameExtractor:
+    """
+    A class used to extract frames from a video file and save them as images.
+
+    Attributes
+    ----------
+    input_folder : str
+        The path to the folder containing the input video files.
+    output_folder : str
+        The path to the folder where the extracted frames and metadata will be saved.
+    use_multithreading : bool, optional
+        Whether to use multithreading to extract frames from multiple videos simultaneously. Default is False.
+    image_format : str, optional
+        The format to use when saving the extracted frames as images. Default is 'jpg'.
+
+    Methods
+    -------
+    process_video(mov_file)
+        Extracts frames from a single video file and saves them as images.
+    extract_frames_and_metadata()
+        Extracts frames and metadata from all video files in the input folder.
+    """
+
     def __init__(self, input_folder, output_folder, use_multithreading=False, image_format='jpg'):
+        """
+        Parameters
+        ----------
+        input_folder : str
+            The path to the folder containing the input video files.
+        output_folder : str
+            The path to the folder where the extracted frames and metadata will be saved.
+        use_multithreading : bool, optional
+            Whether to use multithreading to extract frames from multiple videos simultaneously. Default is False.
+        image_format : str, optional
+            The format to use when saving the extracted frames as images. Default is 'jpg'.
+        """
         self.input_folder = input_folder
         self.output_folder = output_folder
         self.use_multithreading = use_multithreading
         self.image_format = image_format
         logging.basicConfig(filename='video_frame_extraction.log', level=logging.INFO)
 
+
+    def extract_frames_and_metadata(self):
+        """
+        Extracts frames and metadata from all video files in the input folder.
+        """
+        if not os.path.exists(self.output_folder):
+            os.makedirs(self.output_folder)
+            logging.info(f"Created output folder {self.output_folder}")
+
+        mov_files = [f for f in os.listdir(self.input_folder) if f.endswith('.MOV')]
+        logging.info(f"Found {len(mov_files)} .MOV files.")
+
+        with ThreadPoolExecutor() as executor:
+            futures = []
+            for mov_file in mov_files:
+                futures.append(executor.submit(self.process_video, mov_file))
+
+            with tqdm(total=len(futures)) as pbar:
+                for future in futures:
+                    future.result()
+                    pbar.update(1)
+
     def process_video(self, mov_file):
+        """
+        Extracts frames from a single video file and saves them as images.
+
+        Parameters
+        ----------
+        mov_file : str
+            The name of the video file to extract frames from.
+        """
         video_path = os.path.join(self.input_folder, mov_file)
         cap = cv2.VideoCapture(video_path)
 
@@ -29,33 +94,27 @@ class VideoFrameExtractor:
         }
 
         frame_number = 0
-        while True:
-            ret, frame = cap.read()
-            if not ret:
-                break
+        metadata_file = os.path.join(self.output_folder, f"{mov_file}_metadata.json")
+        with open(metadata_file, 'w') as f:
+            json.dump(metadata, f, indent=4)
 
-            frame_file = os.path.join(frames_folder, f"frame_{frame_number}.{self.image_format}")
-            cv2.imwrite(frame_file, frame)
-            frame_number += 1
+        with tqdm(total=metadata['total_frames']) as pbar:
+            while True:
+                ret, frame = cap.read()
+                if not ret:
+                    break
+
+                frame_file = os.path.join(frames_folder, f"frame_{frame_number}.{self.image_format}")
+                cv2.imwrite(frame_file, frame)
+                frame_number += 1
+                pbar.update(1)
+
+        cap.release()
 
         metadata_file = os.path.join(self.output_folder, f"{mov_file}_metadata.json")
         with open(metadata_file, 'w') as f:
             json.dump(metadata, f, indent=4)
 
-        cap.release()
+        
         logging.info(f"Extracted frames and metadata from {mov_file}")
 
-    def extract_frames_and_metadata(self):
-        if not os.path.exists(self.output_folder):
-            os.makedirs(self.output_folder)
-            logging.info(f"Created output folder {self.output_folder}")
-
-        mov_files = [f for f in os.listdir(self.input_folder) if f.endswith('.MOV')]
-        logging.info(f"Found {len(mov_files)} .MOV files.")
-
-        if self.use_multithreading:
-            with ThreadPoolExecutor() as executor:
-                list(tqdm(executor.map(self.process_video, mov_files), total=len(mov_files)))
-        else:
-            for mov_file in tqdm(mov_files, desc="Processing .MOV files"):
-                self.process_video(mov_file)
